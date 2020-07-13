67fb854299c162a2b589f3acc6c528b8de97399a
exgonzalez <exgonzalez@syc.com.co>
2020-07-12 22:11:06 -0500

    Se incluye Entry custom para iOS

---
+++ XamApp/XamApp.iOS/EntryCustomRenderer.cs
@@ -0,0 +1,22 @@
+﻿using UIKit;
+using XamApp.Customs;
+using XamApp.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(EntryCustom), typeof(EntryCustomRenderer))]
+namespace XamApp.iOS
+{
+    public class EntryCustomRenderer : EntryRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (this.Control == null) return;
+
+            this.Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
