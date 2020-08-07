43527dbcb1ccdea79353b6b9ba0aaf3b876c5e2f
exgonzalez <exgonzalez@syc.com.co>
2020-08-07 00:41:46 -0500

    Se agrega banner a la lista

--- XamApp/XamApp/ViewModels/BurgerListViewModel.cs
+++ XamApp/XamApp/ViewModels/BurgerListViewModel.cs
@@ -1,13 +1,26 @@
 ﻿using Refit;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Net.Http;
 using System.Text;
+using XamApp.Models;
 using XamApp.Services;
 
 namespace XamApp.ViewModels
 {
     public class BurgerListViewModel : BaseViewModel
     {
+        private ObservableCollection<Burger> _burgers = new ObservableCollection<Burger>();
+
+        public ObservableCollection<Burger> Burgers
+        {
+            get { return _burgers; }
+            set { _burgers = value;
+                OnPropertyChanged();
+            }
+        }
 
         public BurgerListViewModel()
         {
@@ -16,8 +29,31 @@ namespace XamApp.ViewModels
 
         async void GetResponse()
         {
-            var apiResponse = RestService.For<IBurgersApi>("https://netcorexam.azurewebsites.net/api/v1/NetCoreXamApi");
-            var response = await apiResponse.GetBurgers();
+            try
+            {
+                //var apiResponse = RestService.For<IBurgersApi>("https://netcorexam.azurewebsites.net/api/v1/NetCoreXamApi");
+                //var response = await apiResponse.GetBurgers();
+
+                Burgers = new ObservableCollection<Burger>
+                {
+                    new Burger
+                    {
+                        NombreHamburguesa = "Rock Burger",
+                        Categoria = "Burger - Pizza",
+                        Calificacion = (decimal)4.5,
+                        Rating = 121,
+                        Favorito = true,
+                        Imagen = "bacon-burger"
+                    }
+
+
+                };
+
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.Message);
+            }
         }
     }
 }
